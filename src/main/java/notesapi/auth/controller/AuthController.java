d09c405bca7f17db08368d70d0b4ea819e676483
Paulo Henrique De Sousa Coelho <paulo_henrique006@outlook.com>
2026-09-18 19:19:56 -0300

    feat(jwt): inclui role como claim no token

--- src/main/java/notesapi/auth/controller/AuthController.java
+++ src/main/java/notesapi/auth/controller/AuthController.java
@@ -1,17 +1,21 @@
 package notesapi.auth.controller;
 
+
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import notesapi.auth.dto.LoginRequest;
 import notesapi.auth.dto.LoginResponse;
 import notesapi.security.JwtService;
+import notesapi.user.entity.UserEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/auth")
@@ -25,8 +29,9 @@ public class AuthController {
         UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
                 request.email(), request.password());
 
-        manager.authenticate(authToken);
-        String token = jwtService.generateToken(request.email());
+        Authentication authentication = manager.authenticate(authToken);
+        UserEntity user = (UserEntity) authentication.getPrincipal();
+        String token = jwtService.generateToken(user);
         return ResponseEntity.ok(new LoginResponse(token));
     }
 }
